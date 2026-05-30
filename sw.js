0f065a331ad46acb42a6b82996744fede7a1b8fc
Yahli215 <yahli215@users.noreply.github.com>
2026-05-30 10:25:13 +0300

    SW: force skipWaiting + clear all caches on activate

--- sw.js
+++ sw.js
@@ -1,1 +1,7 @@
+const VERSION = '2';
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => e.waitUntil(
+  caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+    .then(() => self.clients.claim())
+));
 self.addEventListener('fetch', e => e.respondWith(fetch(e.request).catch(() => caches.match(e.request))));
