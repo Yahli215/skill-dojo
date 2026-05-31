f3799ba847cf15cc18b77890c36d0d47b513f691
Yahli215 <yahli215@users.noreply.github.com>
2026-05-31 18:25:42 +0300

    Remove badges section; Pomodoro defaults 90min work / 15min break

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = '3';
+const VERSION = '4';
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(
   caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
