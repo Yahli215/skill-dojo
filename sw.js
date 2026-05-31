bc27ade5ad3510222f5c378980c541ded94cad7d
Yahli215 <yahli215@users.noreply.github.com>
2026-05-31 14:55:29 +0300

    Bump SW version to force cache clear

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = '2';
+const VERSION = '3';
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(
   caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
