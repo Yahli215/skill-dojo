d719daa39702800092c707e081dbc2a00fa09e6c
Yahli215 <yahli215@users.noreply.github.com>
2026-06-02 13:45:50 +0300

    Bump SW to v15 to force cache invalidation on all devices

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = '14';
+const VERSION = '15';
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(
   caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
