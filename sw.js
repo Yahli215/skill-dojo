6c3789b693800209186001dcb6849e9a877f7b50
Yahli215 <yahli215@users.noreply.github.com>
2026-06-03 11:51:27 +0300

    Bump SW to v16, force-push habit migration on load

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = '15';
+const VERSION = '16';
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(
   caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
