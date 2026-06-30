0b9e0f66147d7b4f7722087633889308247a88bd
Yahli215 <yahli215@users.noreply.github.com>
2026-06-30 13:46:40 +0300

    Add distraction/mindset friction tags for non-chess intrusive thoughts

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = '21';
+const VERSION = '22';
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(
   caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
