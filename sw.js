615a845459e9d8eb273ab9277f58e7b57ba10a2d
Yahli215 <yahli215@users.noreply.github.com>
2026-06-30 19:14:59 +0300

    Redesign Current Habit tab as experiment tracker (v25)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = '24';
+const VERSION = '25';
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(
   caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
