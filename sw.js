084bfdaa57e281e9fe1481a6737dcf68db1e55c8
Yahli215 <yahli215@users.noreply.github.com>
2026-07-01 12:39:05 +0300

    Friction log: tag required first, description optional

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = '28';
+const VERSION = '29';
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(
   caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
