d0e718b18f673a9b0eed746d4de8456e9074669c
Yahli215 <yahli215@users.noreply.github.com>
2026-07-01 12:00:35 +0300

    Remove Pre-Game and Session tabs from Insights

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = '26';
+const VERSION = '27';
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(
   caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
