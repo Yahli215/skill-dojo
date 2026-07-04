28dbae5a3333c73d9c67a39feb88221eb0893ac6
Yahli215 <yahli215@users.noreply.github.com>
2026-07-04 15:10:47 +0300

    feat: skill checkpoint expiration + forced resolution (v34)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = '33';
+const VERSION = '34';
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(
   caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
