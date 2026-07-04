6978f4e5796b4261613970430f00d475f0a011ba
Yahli215 <yahli215@users.noreply.github.com>
2026-07-04 15:40:24 +0300

    fix: getBottles ls.get default string (v36)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = '34';
+const VERSION = '36';
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(
   caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
