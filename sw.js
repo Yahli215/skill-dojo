397c0edfaa805ae3afdea86771c44d475cc89560
Yahli215 <yahli215@users.noreply.github.com>
2026-07-06 00:56:56 +0300

    feat: remove Journey grid (v42)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = '41';
+const VERSION = '42';
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(
   caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
