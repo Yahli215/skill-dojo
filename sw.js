b5b7113fabde9e7b28f4eb8269d53460fc393db7
Yahli215 <yahli215@users.noreply.github.com>
2026-07-06 00:47:25 +0300

    feat: simple habit list, remove experiment system (v40)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = '39';
+const VERSION = '40';
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(
   caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
