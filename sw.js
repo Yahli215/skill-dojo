cc7c374a80cb5baf3763ba339f8140a3e63d0874
Yahli215 <yahli215@users.noreply.github.com>
2026-07-06 12:26:39 +0300

    feat: split completed into Automatic/Retired, add retire button (v45)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = '44';
+const VERSION = '45';
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(
   caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
