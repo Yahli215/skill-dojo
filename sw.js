1243d82dbea90dfee9d54ccce2df68e38dbdbb77
Yahli215 <yahli215@users.noreply.github.com>
2026-07-09 11:33:06 +0300

    feat: full-width check bar, cleaner habit cards (v49)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = '48';
+const VERSION = '49';
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(
   caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
