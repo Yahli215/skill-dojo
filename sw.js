648a4840fab697e5a5ae9c535d7b6b0a60ad6a77
Yahli215 <yahli215@users.noreply.github.com>
2026-07-15 13:03:55 +0300

    feat: Disturbing Thoughts Challenge Space, thought FAB, Mind tab (v51)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = '50';
+const VERSION = '51';
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(
   caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
