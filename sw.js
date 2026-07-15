8cf7e850142b5742b50e2fed851e0130a10499c1
Yahli215 <yahli215@users.noreply.github.com>
2026-07-15 12:56:43 +0300

    fix: friction tag optional, Log it always enabled (v50)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = '49';
+const VERSION = '50';
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(
   caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
