325d66f0675bb7cbd34c10f9498ee2eaedd0377c
Yahli215 <yahli215@users.noreply.github.com>
2026-08-19 20:34:28 +0300

    fix: disable opening file check popup (v57)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = '56';
+const VERSION = '57';
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(
   caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
