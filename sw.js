fc2d3d30fba9b8f87316c5849d8abbede4e704b9
Yahli215 <yahli215@users.noreply.github.com>
2026-09-11 16:12:20 +0300

    Lifebot: filter phantom sessions, completion rate only from v66+ data (v67)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = '66';
+const VERSION = '67';
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(
   caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
