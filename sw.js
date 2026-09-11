f29b02513af70650dcb6db109ba0efad0cbeeff1
Yahli215 <yahli215@users.noreply.github.com>
2026-09-11 16:07:46 +0300

    Lifebot: session outcomes, break tagging, completion/break stats tab (v66)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = '65';
+const VERSION = '66';
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(
   caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
